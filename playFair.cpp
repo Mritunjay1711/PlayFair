552513919c65dee99ff5823be980ab0c881a7e07
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2022-09-25 18:58:41 +0530

    new

--- playFair.cpp
+++ playFair.cpp
@@ -1,5 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
 //Add your functions like this
-void initArray(int arr[5][5])
+void initArray(int arr[5][5], string key)
 {
+
+}
+
+int main(){
     
+    return 0;
 }
