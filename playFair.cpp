6778030bc919f9547692ee01b35116181c4d0ec9
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2022-09-25 19:55:16 +0530

    new

--- playFair.cpp
+++ playFair.cpp
@@ -8,6 +8,26 @@ void initArray(int arr[5][5], string key)
 
 }
 
+pair<int, int> findIndex(int arr[5][5], char ch)
+{
+    pair<int, int> index;
+    if(ch == 'J')
+        ch == 'I';
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if(ch == arr[i][j])
+            {
+                index = make_pair(i, j);
+                break;
+            }
+        }
+    }
+    return index;
+}
+
 int main(){
     
     return 0;
