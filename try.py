507e17627f3f5583efb93358befbe8eb160d3f4d
divyansi01 <divyansim01@gmail.com>
2022-10-04 09:33:31 +0530

    new

--- try.py
+++ try.py
@@ -1,7 +1,7 @@
+from queue import Queue
+from collections import deque
 plainText = input('Enter the plainText\n')
-print(plainText)
 key = input('Enter the key\n')
-print(key)
 plainText = plainText.upper()
 key = key.upper()
 
@@ -12,11 +12,34 @@ def remove(plainText):
 
 plainText = remove(plainText)
 
-rows,cols = (5, 5)
-arr = [[0] * cols] * rows
+
+arr = []
 
 
 def keyMatrix(arr, key):
     N = 26
     alpha = [0] * N
-    
+    queue = []
+
+    for i in key:
+        temp = ord(i) - ord('A')
+        if i != 'J' and alpha[temp] != 1:
+            alpha[temp] = 1
+            queue.append(i)
+    for i in range(len(alpha)):
+        if alpha[i] != 1:
+            temp = chr(i + ord('A'))
+            if temp != 'J':
+                queue.append(temp)
+
+    for i in range(5):
+        row = []
+        for j in range(5):
+            temp = queue.pop(0)
+            print(temp)
+            row.append(temp)
+        arr.append(row)
+
+
+keyMatrix(arr, key)
+print(arr)
