1afbf0d0d6583dd815b7edf5a6c1cfb4b4448cfc
divyansi01 <divyansim01@gmail.com>
2022-10-04 12:28:30 +0530

    new

--- try.py
+++ try.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from collections import deque
+from tkinter import N
 plainText = input('Enter the plainText\n')
 key = input('Enter the key\n')
 plainText = plainText.upper()
@@ -11,7 +12,7 @@ def remove(plainText):
 
 
 plainText = remove(plainText)
-
+cypherText = ''
 arr = []
 
 
@@ -35,21 +36,45 @@ def keyMatrix(arr, key):
         row = []
         for j in range(5):
             temp = queue.pop(0)
-            print(temp, end = " ")
+            print(temp, end=" ")
             row.append(temp)
         print()
         arr.append(row)
+
 
 def findIndex(arr, ch):
     for i, row in enumerate(arr):
         for j, col in enumerate(row):
             if arr[i][j] == ch:
-                return i, j;
+                return i, j
+
+
+def encryption(arr, plainText, cypherText):
+    i = 0
+    n = len(plainText)
+    while i < n:
+        ind1i, ind1j = findIndex(arr, plainText[i])
+        i = i + 1
+        if i == n:
+            ind2i, ind2j = findIndex(arr, 'X')
+            i = i + 1
+        else:
+            ind2i, ind2j = findIndex(arr, plainText[i])
+            i = i + 1
+        if ind1i == ind2i:
+            cypherText = cypherText + arr[ind1i][(ind1j + 1) % 5]
+            cypherText = cypherText + arr[ind2i][(ind2j + 1) % 5]
+        elif ind1j == ind2j:
+            cypherText = cypherText + arr[(ind1i + 1) % 5][ind1j]
+            cypherText = cypherText + arr[(ind2i + 1) % 5][ind2j]
+        else:
+            cypherText = cypherText + arr[ind1i][ind2j]
+            cypherText = cypherText + arr[ind2i][ind1j]
 
 
 keyMatrix(arr, key)
-ch = 'A'
-indi, indj=findIndex(arr, ch)
-print(indi, indj)
-print(arr)
-
+# ch = 'A'
+# indi, indj = findIndex(arr, ch)
+# print(indi, indj)
+encryption(arr, plainText, cypherText)
+print("Cipher text", cypherText)
